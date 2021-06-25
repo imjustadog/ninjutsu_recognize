538ff40288f617e8270b570b71708b65b6b2c9ab
imjustadog <imjustadog@126.com>
2021-06-26 00:37:17 +0800

    add fox

--- effect_overlay.cpp
+++ effect_overlay.cpp
@@ -3,11 +3,12 @@
 const int WATER_RMAX = 10;
 const int WATER_RMIN = 5;
 const int WATER_COUNT = 30;
-const int CHANGE_INTERVAL = 4;
+const int WATER_INTERVAL = 4;
 const int SEED = 666;
 const int TIME_TRANS = 40;
 const int TIME_MOVE = 60;
 const int TIME_STOP = 100;
+const int FOX_INTERVAL = 30;
 
 effect_overlay::effect_overlay()
 {
@@ -17,7 +18,7 @@ effect_overlay::~effect_overlay()
 {
 }
 
-int effect_overlay::draw_water(cv::Mat frame, cv::Point hand_center, int flag_restart)
+int effect_overlay::draw_water(cv::Mat &frame, cv::Point hand_center, int flag_restart)
 {
   static vector<vector<int>> water_kernel(WATER_COUNT + 1, vector<int>(3, 0));
   static int current_time = 0;
@@ -45,14 +46,14 @@ int effect_overlay::draw_water(cv::Mat frame, cv::Point hand_center, int flag_re
   {
     if(current_time < TIME_TRANS)
     {
-      ellipse_x = min(current_time / CHANGE_INTERVAL, water_kernel[i][2]);
+      ellipse_x = min(current_time / WATER_INTERVAL, water_kernel[i][2]);
       ellipse_y = ellipse_x;
       point_x = water_kernel[i][0];
       point_y = water_kernel[i][1];
     }
     else if(current_time < TIME_MOVE)
     {
-      ellipse_x = max(1, water_kernel[i][2] - (current_time - TIME_TRANS) / CHANGE_INTERVAL);
+      ellipse_x = max(1, water_kernel[i][2] - (current_time - TIME_TRANS) / WATER_INTERVAL);
       ellipse_y = 2 * water_kernel[i][2] - ellipse_x;
       point_x = water_kernel[i][0];
       point_y = water_kernel[i][1];
@@ -61,8 +62,8 @@ int effect_overlay::draw_water(cv::Mat frame, cv::Point hand_center, int flag_re
     {
       ellipse_x = 1;
       ellipse_y = 2 * water_kernel[i][2] + (current_time - TIME_MOVE) / 3;
-      point_x = water_kernel[i][0] + CHANGE_INTERVAL * (current_time - TIME_MOVE) * sin((270 - 180 / WATER_COUNT * i) * M_PI / 180.0);
-      point_y = water_kernel[i][1] - CHANGE_INTERVAL * (current_time - TIME_MOVE) * cos((270 - 180 / WATER_COUNT * i) * M_PI / 180.0);
+      point_x = water_kernel[i][0] + WATER_INTERVAL * (current_time - TIME_MOVE) * sin((270 - 180 / WATER_COUNT * i) * M_PI / 180.0);
+      point_y = water_kernel[i][1] - WATER_INTERVAL * (current_time - TIME_MOVE) * cos((270 - 180 / WATER_COUNT * i) * M_PI / 180.0);
     }
     else
     {
@@ -85,10 +86,11 @@ int effect_overlay::draw_water(cv::Mat frame, cv::Point hand_center, int flag_re
   return 0;
 }
 
-int effect_overlay::draw_lightning(cv::Mat frame, cv::Point hand_center, int flag_restart)
+int effect_overlay::draw_lightning(cv::Mat &frame, cv::Point hand_center, int flag_restart)
 {
   static int current_time = 0;
   static cv::Mat lightning_spark = cv::imread("spark.png");
+  cv::Mat resized_spark;
   cv::Mat roi_spark;
   cv::Point spark_coord;
   int xmin = 0;
@@ -101,44 +103,54 @@ int effect_overlay::draw_lightning(cv::Mat frame, cv::Point hand_center, int fla
     current_time = 0;
   }
 
-  spark_coord.x = hand_center.x - lightning_spark.cols / 2;
-  spark_coord.y = hand_center.y - lightning_spark.rows / 2;
+  if(current_time % 4 == 1)
+    cv::flip(lightning_spark, resized_spark, 1);
+  else if(current_time % 4 == 2)
+    cv::flip(lightning_spark, resized_spark, 0);
+  else if(current_time % 4 == 3)
+    cv::transpose(lightning_spark, resized_spark);
+  else
+    lightning_spark.copyTo(resized_spark);
+
+  spark_coord.x = hand_center.x - resized_spark.cols / 2;
+  spark_coord.y = hand_center.y - resized_spark.rows / 2;
 
   if(spark_coord.x < 0)
   {
     xmin = 0 - spark_coord.x;
     spark_coord.x = 0;
   }
-  if(spark_coord.x > frame.cols - lightning_spark.cols)
+  else if(spark_coord.x > frame.cols - resized_spark.cols)
   {
-    xmax = lightning_spark.cols - (frame.cols - spark_coord.x);
-    spark_coord.x = frame.cols - lightning_spark.cols;
+    xmax = resized_spark.cols - (frame.cols - spark_coord.x);
   }
+  else if(spark_coord.x > frame.cols)
+  {
+    return 1;
+  }
+
+
   if(spark_coord.y < 0)
   {
     ymin = 0 - spark_coord.y;
     spark_coord.y = 0;
   }
-  if(spark_coord.y > frame.rows - lightning_spark.rows)
+  else if(spark_coord.y > frame.rows - resized_spark.rows)
   {
-    ymax = lightning_spark.rows - (frame.rows - spark_coord.y);
-    spark_coord.y = frame.rows - lightning_spark.rows;
+    ymax = resized_spark.rows - (frame.rows - spark_coord.y);
+  }
+  else if(spark_coord.y > frame.rows)
+  {
+    return 1;
   }
 
   //cout << " xmin: " << xmin << " ymin: " << ymin << " xmax: " << xmax << " ymax: " << ymax << endl;
   //cout << " x: " << spark_coord.x << " y: " << spark_coord.y << endl;
   //cout << frame.cols << " " << frame.rows << endl;
-
-  if(current_time % 4 == 1)
-    cv::flip(lightning_spark, lightning_spark, 1);
-  else if(current_time % 4 == 2)
-    cv::transpose(lightning_spark, lightning_spark);
-  else if(current_time % 4 == 3)
-    cv::flip(lightning_spark, lightning_spark, 0);
     
   roi_spark = frame(cv::Rect(spark_coord.x, spark_coord.y, xmax - xmin, ymax - ymin));
 
-  cv::addWeighted(roi_spark, 1.0, lightning_spark(cv::Rect(xmin, ymin, xmax - xmin, ymax - ymin)), 0.5, 0, roi_spark, -1);
+  cv::addWeighted(roi_spark, 1.0, resized_spark(cv::Rect(xmin, ymin, xmax - xmin, ymax - ymin)), 0.5, 0, roi_spark, -1);
 
   current_time ++;
 
@@ -150,7 +162,108 @@ int effect_overlay::draw_lightning(cv::Mat frame, cv::Point hand_center, int fla
   return 0;
 }
 
-int effect_overlay::add_effect(string current_gesture, cv::Mat frame, cv::Point hand_center, int flag_restart)
+int effect_overlay::draw_fox(cv::Mat &frame, cv::Point hand_center, int flag_restart)
+{
+  static int current_time = 0;
+  static cv::Mat fox = cv::imread("fox.png", CV_LOAD_IMAGE_UNCHANGED);
+  cv::Mat resized_fox;
+  cv::Mat roi_fox;
+  cv::Point fox_coord;
+  int current_width;
+  int current_height;
+
+  if(flag_restart)
+  {
+    current_time = 0;
+  }
+
+  //if(current_time % 4 == 1)
+  //  cv::flip(fox, resized_fox, 0);
+  //else if(current_time % 4 == 2)
+  //  cv::transpose(fox, resized_fox);
+  //else if(current_time % 4 == 3)
+  //  cv::flip(fox, resized_fox, 1);
+  //else
+  //  fox.copyTo(resized_fox);
+
+  current_height = fox.rows + current_time * FOX_INTERVAL;
+  current_width = fox.cols + current_time * FOX_INTERVAL * fox.cols / fox.rows;
+  cv::resize(fox,resized_fox,cv::Size(current_width,current_height),CV_INTER_AREA);		
+
+  int xmin = 0;
+  int xmax = resized_fox.cols;
+  int ymin = 0;
+  int ymax = resized_fox.rows;
+
+  fox_coord.x = hand_center.x - resized_fox.cols / 2;
+  fox_coord.y = hand_center.y - resized_fox.rows / 2;
+
+  if(fox_coord.x < 0)
+  {
+    xmin = 0 - fox_coord.x;
+    fox_coord.x = 0;
+  }
+  else if(fox_coord.x > frame.cols - resized_fox.cols)
+  {
+    xmax = resized_fox.cols - (frame.cols - fox_coord.x);
+  }
+  else if(fox_coord.x > frame.cols)
+  {
+    return 1;
+  }
+
+  if(fox_coord.y < 0)
+  {
+    ymin = 0 - fox_coord.y;
+    fox_coord.y = 0;
+  }
+  else if(fox_coord.y > frame.rows - resized_fox.rows)
+  {
+    ymax = resized_fox.rows - (frame.rows - fox_coord.y);
+  }
+  else if(fox_coord.y > frame.rows)
+  {
+    return 1;
+  }
+
+  if(xmax - xmin > frame.cols)
+  {
+    xmax = xmin + frame.cols;
+  }
+  if(ymax - ymin > frame.rows)
+  {
+    ymax = ymin + frame.rows;
+  }
+
+  //cout << " xmin: " << xmin << " ymin: " << ymin << " xmax: " << xmax << " ymax: " << ymax << endl;
+  //cout << " x: " << fox_coord.x << " y: " << fox_coord.y << endl;
+
+  roi_fox = frame(cv::Rect(fox_coord.x, fox_coord.y, xmax - xmin, ymax - ymin));
+
+  vector<cv::Mat> roi_channels;    
+  vector<cv::Mat> fox_channels;    
+  cv::split(roi_fox, roi_channels);    
+  cv::split(resized_fox(cv::Rect(xmin, ymin, xmax - xmin, ymax - ymin)), fox_channels); 
+
+  for (int i = 0; i < 3; i++)    
+  {    
+    roi_channels[i] = roi_channels[i].mul(255 - fox_channels[3], 1 / 255.0);    
+    roi_channels[i] += fox_channels[i].mul(fox_channels[3], 1 / 255.0);    
+  }
+
+  cv::merge(roi_channels, roi_fox);
+
+  current_time ++;
+
+  if(current_time >= TIME_STOP)
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+int effect_overlay::add_effect(string current_gesture, cv::Mat &frame, cv::Point hand_center, int flag_restart)
 {
   int ret;
   ret = (this->*seq_map[current_gesture])(frame, hand_center, flag_restart);
